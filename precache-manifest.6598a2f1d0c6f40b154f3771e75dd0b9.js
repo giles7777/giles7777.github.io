c81f68dea8e35ac95c3e1c940a19f88ea85648ef
Alan Hudson <giles7777@gmail.com>
2020-04-30 11:04:02 -0700

    Updates

---
+++ precache-manifest.6598a2f1d0c6f40b154f3771e75dd0b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4950bedba16f3bc9f27e0aa0b8aa3f0a",
+    "url": "/giles7777.github.io/build/index.html"
+  },
+  {
+    "revision": "5aead22805dd2c38fc63",
+    "url": "/giles7777.github.io/build/static/css/2.05dd90ba.chunk.css"
+  },
+  {
+    "revision": "f88413f13b5e63439599",
+    "url": "/giles7777.github.io/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5aead22805dd2c38fc63",
+    "url": "/giles7777.github.io/build/static/js/2.d4358af3.chunk.js"
+  },
+  {
+    "revision": "06a21007695b1470482b5824a47c30af",
+    "url": "/giles7777.github.io/build/static/js/2.d4358af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88413f13b5e63439599",
+    "url": "/giles7777.github.io/build/static/js/main.efdf5e02.chunk.js"
+  },
+  {
+    "revision": "5b60cc31b017963a8c8a",
+    "url": "/giles7777.github.io/build/static/js/runtime-main.a6b4d0ad.js"
+  }
+]);
