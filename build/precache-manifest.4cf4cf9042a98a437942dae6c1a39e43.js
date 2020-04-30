25b9a6fcb6c18b6d352fe8c88d583ad52587498d
Alan Hudson <giles7777@gmail.com>
2020-04-30 10:48:26 -0700

    initial react app

---
+++ build/precache-manifest.4cf4cf9042a98a437942dae6c1a39e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502ddd54011b219bb17ebd8d5704ad58",
+    "url": "/giles7777.github.io/index.html"
+  },
+  {
+    "revision": "5aead22805dd2c38fc63",
+    "url": "/giles7777.github.io/static/css/2.05dd90ba.chunk.css"
+  },
+  {
+    "revision": "61761ad88cb4e3780b61",
+    "url": "/giles7777.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5aead22805dd2c38fc63",
+    "url": "/giles7777.github.io/static/js/2.d4358af3.chunk.js"
+  },
+  {
+    "revision": "06a21007695b1470482b5824a47c30af",
+    "url": "/giles7777.github.io/static/js/2.d4358af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61761ad88cb4e3780b61",
+    "url": "/giles7777.github.io/static/js/main.12615894.chunk.js"
+  },
+  {
+    "revision": "426a5c518f70fe28037c",
+    "url": "/giles7777.github.io/static/js/runtime-main.19c6ef03.js"
+  }
+]);
